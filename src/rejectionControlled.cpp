6a39f57657a021aa8840a1243734064a17ab1630
Pedro Baldoni <pedrobaldoni@gmail.com>
2020-12-19 17:09:09 +0100

    Computes RC re-weighting

--- src/rejectionControlled.cpp
+++ src/rejectionControlled.cpp
@@ -11,8 +11,21 @@ using namespace H5;
 
 // Applies reweight.cpp and aggregate.cpp for rejection-controlled posterior probabilities
 //[[Rcpp::export]]
-arma::mat rejectionControlled(NumericVector x,arma::vec f,double p) { 
+arma::field<arma::mat> rejectionControlled(Rcpp::StringVector nameMarginalProb,arma::vec f,double p) { 
+    arma::mat logProb1;
+    arma::vec reWeights;
+
+    std::vector<std::string> vstrings(1);
+    vstrings[0] = nameMarginalProb(0);
+    logProb1.load(vstrings[0]);
     
-    arma::vec reWeights = reweight(x,p); // Reweights posterior probabilities
-    return aggregate(reWeights,f); // Aggregating
+    int K = logProb1.n_cols; //Number of states
+    arma::field<mat> aggReWeights(K,1);
+
+    for(int k = 0; k < K; k++){
+        reWeights = reweight(exp(logProb1.col(k)),p);
+        aggReWeights(k,0) = aggregate(reWeights,f);
+    }
+
+    return aggReWeights;
 }
