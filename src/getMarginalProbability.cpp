6d3ffca4287af9929da8178b03c3e0528484e98c
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-01-01 15:24:06 -0300

    Function to extract marginal probabilities

--- src/getMarginalProbability.cpp
+++ src/getMarginalProbability.cpp
@@ -9,13 +9,13 @@ using namespace H5;
 
 // Get marginal probabilities saved in disk
 //[[Rcpp::export]]
-arma::mat getMarginalProbability(Rcpp::StringVector nameMarginalProb) { 
+arma::mat getMarginalProbability(Rcpp::StringVector hdf5) { 
     
     arma::mat logProb1;
     
     std::vector<std::string> vstrings(1);
-    vstrings[0] = nameMarginalProb(0);
-    logProb1.load(vstrings[0]);
+    vstrings[0] = hdf5(0);
+    logProb1.load(hdf5_name(vstrings[0], "logProb1"));
     
     return exp(logProb1);
 }
