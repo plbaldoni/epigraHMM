2a873327970a7d2f6bd95ce5645e027f965508ec
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-03-01 08:30:50 +0100

    Adding example and tidying code

--- R/addOffsets.R
+++ R/addOffsets.R
@@ -16,13 +16,28 @@
 #'
 #' @importFrom SummarizedExperiment assayNames assay
 #'
+#' @examples 
+#' 
+#' # Creating dummy object
+#' countData <- list('counts' = matrix(rpois(4e5,10),ncol = 4),
+#' 'controls' = matrix(rpois(4e5,5),ncol = 4))
+#' colData <- data.frame(condition = c('A','A','B','B'), replicate = c(1,2,1,2))
+#' object <- epigraHMMDataSetFromMatrix(countData,colData)
+#'
+#' # Adding pre-computed offsets
+#' object <- addOffsets(object = object,
+#'                      offsets = matrix(rnorm(4e5),ncol = 4))
+#'
 #' @export
-addOffsets <- function(object,offsets){
-    if('offsets' %in% SummarizedExperiment::assayNames(object)){
-        SummarizedExperiment::assay(object,'offsets') <- SummarizedExperiment::assay(object,'offsets') + offsets
+addOffsets <- function(object, offsets) {
+    if ('offsets' %in% SummarizedExperiment::assayNames(object)) {
+        SummarizedExperiment::assay(object, 'offsets') <-
+            SummarizedExperiment::assay(object, 'offsets') + offsets
     } else{
-        dimnames(offsets) <- dimnames(SummarizedExperiment::assay(object,'counts'))
-        SummarizedExperiment::assay(object,'offsets',withDimnames=TRUE) <- offsets
+        dimnames(offsets) <-
+            dimnames(SummarizedExperiment::assay(object, 'counts'))
+        SummarizedExperiment::assay(object, 'offsets', withDimnames = TRUE) <-
+            offsets
     }
     return(object)
 }
