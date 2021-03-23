90c24d4c45a5cf251df565fadd5d5996ed40d96a
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-03-23 15:07:06 +0100

    Adding peak check in plotPatterns Changing example

--- R/plotPatterns.R
+++ R/plotPatterns.R
@@ -4,6 +4,7 @@
 #'
 #' @param object an epigraHMMDataSet
 #' @param ranges a GRanges object or a pair of integers with the genomic corrdinates/windows to be plotted
+#' @param peaks either a GRanges object or a vector of logicals (with length equal to the number of rows in `object`) specifying the genomic corrdinates/windows with peaks
 #' @param hdf5 a character string with the hdf5 file path from `epigraHMM`
 #' @param colors an optional argument that specifies the colors for each differential combinatorial pattern
 #'
@@ -11,16 +12,20 @@
 #' 
 #' @examples 
 #' # Creating dummy object
-#' countData <- cbind(rbind(matrix(rnbinom(1e2,mu = 2,size = 10),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 7.5,size = 5),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 7.5,size = 5),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 7.5,size = 5),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 2,size = 10),ncol = 1)),
-#'                    rbind(matrix(rnbinom(1e2,mu = 2,size = 10),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 7.5,size = 5),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 7.5,size = 5),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 2,size = 10),ncol = 1),
-#'                          matrix(rnbinom(1e2,mu = 2,size = 10),ncol = 1)))
+#' countData <- cbind(rbind(matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1)),
+#'                    rbind(matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+#'                          matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1)))
 #' 
 #' colData <- data.frame(condition = c('A','B'), replicate = c(1,1))
 #' rowRanges <- GenomicRanges::GRanges('chrA',
@@ -41,7 +46,9 @@
 #'                   method = 'viterbi')
 #' 
 #' # Plotting patterns
-#' plotPatterns(object,peaks,S4Vectors::metadata(object)$output)
+#' plotPatterns(object,
+#'              ranges = peaks[1],
+#'              peaks = peaks)
 #'
 #' @author Pedro L. Baldoni, \email{pedrobaldoni@gmail.com}
 #' @references \url{https://github.com/plbaldoni/epigraHMM}
@@ -52,6 +59,7 @@
 #' @export
 plotPatterns = function(object,
                         ranges,
+                        peaks,
                         hdf5 = metadata(object)$output,
                         colors = NULL) {
 
@@ -79,6 +87,10 @@ plotPatterns = function(object,
     sub_object <- object[sub_index,]
     sub_postprob <- postprob[sub_index,]
     colnames(sub_postprob) <- patterns
+    
+    # Checking overlaps with differential peaks
+    diff_index <- overlapsAny(sub_object, peaks)
+    sub_postprob[!diff_index,] <- NA
     
     # Annotation 
     if(is.null(colors)){
