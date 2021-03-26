c80255419c239a1d28a6e2470c2f2ba02611ff21
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-03-26 15:27:07 +0100

    Adding log argument for log transformation

--- R/cleanCounts.R
+++ R/cleanCounts.R
@@ -9,6 +9,7 @@
 #' Examples of byNames assays include the 'peaks' assay from `initializer()`. In this case, models will be fit separately for peaks and non-peaks regions.
 #' This can be usefor for effects such as GC content, which are known to have a differential effect between peaks and non-peak regions.
 #' Default is NULL, i.e., effects will be removed without stratification.
+#' @param log a logical indicating if the effect from `effectNames` should be log-transformed in the regression model (default is TRUE)
 #'
 #' @details
 #'
@@ -50,7 +51,7 @@
 #' \url{https://github.com/plbaldoni/epigraHMM}
 #'
 #' @export
-cleanCounts <- function(object,effectNames,byNames = NULL){
+cleanCounts <- function(object,effectNames,byNames = NULL,log = TRUE){
 
     # Checking input
     if (!((methods::is(object)[1]%in%c('SummarizedExperiment','RangedSummarizedExperiment')) &
@@ -72,6 +73,11 @@ cleanCounts <- function(object,effectNames,byNames = NULL){
                           X = do.call(cbind,lapply(seq_len(length(effectNames)),function(w){assay(object,effectNames[w])[,x]})),
                           by = if (is.null(byNames)) rep(TRUE,nrow(object)) else assay(object,byNames)[,x],
                           offsets = if ('offsets' %in% assayNames(object)) assay(object,'offsets')[,x] else rep(0,nrow(object)))
+        
+        ## Log-transform
+        if (log == TRUE){
+            mat$X <- log1p(mat$X)
+        }
 
         ## Writing model formula
         form <- stats::as.formula(paste('y ~',paste0('splines::ns(',colnames(mat)[grep('^X.*',colnames(mat))],', df = 2)',collapse = ' + ')))
