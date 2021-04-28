e4d0bb2022eb6dd5e303df3f425dd248e0ea2aa7
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-04-28 13:01:08 +0200

    Testing pattern plots

---
+++ tests/testthat/test-plotPatterns.R
@@ -0,0 +1,43 @@
+test_that("check if plot is working as it should",{
+    
+    # Creating dummy object
+    countData <- cbind(rbind(matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1)),
+                       rbind(matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 10, size = 5), ncol = 1),
+                             matrix(rnbinom(1e3, mu = 1, size = 10), ncol = 1)))
+    
+    colData <- data.frame(condition = c('A','B'), replicate = c(1,1))
+    rowRanges <- GenomicRanges::GRanges('chrA',
+                                        IRanges::IRanges(start = seq(1,by = 500,
+                                                                     length.out = nrow(countData)),width = 500))
+    
+    object <- epigraHMMDataSetFromMatrix(countData,colData,rowRanges = rowRanges)
+    
+    # Initializing
+    object <- initializer(object,controlEM())
+    
+    # Running epigraHMM
+    object <- epigraHMM(object,controlEM(),type = 'differential',dist = 'nb')
+    
+    # Calling peaks
+    peaks <- callPeaks(object = object,
+                       hdf5 = S4Vectors::metadata(object)$output,
+                       method = 'viterbi')
+    
+    # Plotting patterns
+    fig <- plotPatterns(object,
+                        ranges = peaks[1],
+                        peaks = peaks)
+    
+    expect_equal(methods::is(fig),'pheatmap')
+})
