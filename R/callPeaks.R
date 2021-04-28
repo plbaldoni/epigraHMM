f3922125a200b5a43bb7a1524f42e73908c0fedd
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-04-28 12:58:03 +0200

    Breaking functions for writing files

--- R/callPeaks.R
+++ R/callPeaks.R
@@ -94,55 +94,33 @@ callPeaks = function(object,
         gr.bed$thickEnd <- GenomicRanges::end(gr.bed)
         
         chrset <- as.character(unique(SummarizedExperiment::seqnames(SummarizedExperiment::rowRanges(object))))
-
-        filenames <- vapply(file.path(path.expand(control[['tempDir']]),paste0(control[['fileName']],'_',c('peaks.bed',paste0('prob_',chrset,'.wig')))),checkPath,FUN.VALUE = 'character',USE.NAMES = FALSE)
+        
+        filePath <- file.path(path.expand(control[['tempDir']]),paste0(control[['fileName']],'_',c('peaks.bed',paste0('prob_',chrset,'.wig'))))
+        filenames <- vapply(filePath,checkPath,FUN.VALUE = 'character',USE.NAMES = FALSE)
         names(filenames) <- c('peaks',paste0('prob_',chrset))
 
         # Writing bed file with peaks
         dt.bed <- data.frame(chrom=SummarizedExperiment::seqnames(gr.bed),chromStart=SummarizedExperiment::start(gr.bed),
                              chromEnd=SummarizedExperiment::end(gr.bed),name=gr.bed$name,score=gr.bed$score,strand='.',
                              thickStart=gr.bed$thickStart,thickEnd=gr.bed$thickEnd)
-
-        utils::write.table(dt.bed,file=file.path(control[['tempDir']],"temp.bed"),row.names = FALSE,col.names = FALSE,quote = FALSE,sep="\t")
-        header1 <- paste0('track name="epigraHMM" description="',ifelse(method == 'viterbi','Viterbi Peaks',paste0('FDR-controlled Peaks (FDR = ',method,')')),'" visibility=1 useScore=1')
-        header2 <- paste0('browser position ',dt.bed[1,'chrom'],':',dt.bed[1,'chromStart'],'-',dt.bed[1,'chromEnd'])
-
-        system2('echo',paste0(header2,' | cat - ',file.path(control[['tempDir']],"temp.bed"),' > ',file.path(control[['tempDir']],"temp1.bed")))
-        system2('echo',paste0(header1,' | cat - ',file.path(control[['tempDir']],"temp1.bed"),' > ',as.character(filenames['peaks'])))
-        system2('rm',paste(file.path(control[['tempDir']],"temp.bed"),file.path(control[['tempDir']],"temp1.bed")))
+        
+        writeBed(dt.bed,control,method,filenames)
 
         # Writing wig files with posterior probabilities
         dt.bigwig <- data.frame(chrom=SummarizedExperiment::seqnames(SummarizedExperiment::rowRanges(object)),
                                 chromStart=SummarizedExperiment::start(SummarizedExperiment::rowRanges(object)),
                                 chromEnd=SummarizedExperiment::end(SummarizedExperiment::rowRanges(object)),
                                 prob=prob)
-
-        for(i in chrset){
-            dt.bigwig.subset <- dt.bigwig[dt.bigwig$chrom==i,]
-
-            utils::write.table(dt.bigwig.subset[,c('chromStart','prob')],
-                               file=file.path(control[['tempDir']],"temp.bed"),row.names = FALSE,col.names = FALSE,quote = FALSE,sep="\t")
-            header1 = paste0('track type=wiggle_0 name="epigraHMM(prob:',i,')" description="','Probability','" visibility=full maxHeightPixels=128:32:11 graphType=bar autoScale=off alwaysZero=on viewLimits=0.0:1.0')
-            header2 = paste0('browser position ',dt.bigwig.subset[1,'chrom'],':',dt.bigwig.subset[1,'chromStart'],'-',dt.bigwig.subset[10,'chromStart'])
-            header3 = paste0('variableStep chrom=',i)
-
-            system2('echo',paste0(header3,' | cat - ',file.path(control[['tempDir']],"temp.bed"),' > ',file.path(control[['tempDir']],"temp1.bed")))
-            system2('echo',paste0(header1,' | cat - ',file.path(control[['tempDir']],"temp1.bed"),' > ',file.path(control[['tempDir']],"temp2.bed")))
-            system2('echo',paste0(header2,' | cat - ',file.path(control[['tempDir']],"temp2.bed"),' > ',as.character(filenames[paste0('prob_',i)])))
-            system2('rm',paste(file.path(control[['tempDir']],"temp.bed"),file.path(control[['tempDir']],"temp1.bed"),file.path(control[['tempDir']],"temp2.bed")))
-
-            tryCatch({
-                rtracklayer::wigToBigWig(x = filenames[paste0('prob_',i)],seqinfo = GenomeInfoDb::seqinfo(SummarizedExperiment::rowRanges(object)))
-                system2("rm",filenames[paste0('prob_',i)])
-            },error = function(x){message("It was not possible to convert wig files to BigWig format because the input object has no specified genome")})
-        }
+        
+        writeWig(object,chrset,dt.bigwig,control,filenames)
         rm(dt.bigwig)
 
         # Writing bedGraph files for mixture probabilities
         if(length(unique((colData(object)[['condition']])))>1){
 
             mixProbSet <- rhdf5::h5read(hdf5,'mixturePatterns')
-            filenames <- c(filenames,vapply(file.path(path.expand(control[['tempDir']]),paste0(control[['fileName']],'_',paste0('mixProb_',mixProbSet,'.wig'))),checkPath,FUN.VALUE = 'character',USE.NAMES = FALSE))
+            wigPath <- file.path(path.expand(control[['tempDir']]),paste0(control[['fileName']],'_',paste0('mixProb_',mixProbSet,'.wig')))
+            filenames <- c(filenames,vapply(wigPath,checkPath,FUN.VALUE = 'character',USE.NAMES = FALSE))
             names(filenames)[names(filenames)==""] <- mixProbSet
 
             for(i in seq_len(length(mixProbSet))){
@@ -150,14 +128,8 @@ callPeaks = function(object,
                 dt.bedgraph = data.frame(chrom=SummarizedExperiment::seqnames(gr.graph),chromStart=SummarizedExperiment::start(gr.graph),
                                          chromEnd=SummarizedExperiment::end(gr.graph),
                                          mixProb=rhdf5::h5read(hdf5,'mixtureProb')[peakindex,i])
-
-                utils::write.table(dt.bedgraph,file=file.path(control[['tempDir']],"temp.bed"),row.names = FALSE,col.names = FALSE,quote = FALSE,sep="\t")
-                header1 = paste0('track type=bedGraph name="epigraHMM(mixProb:',mixProbSet[i],')" description="','Probability','" visibility=full maxHeightPixels=128:32:11 graphType=bar autoScale=off alwaysZero=on viewLimits=0.0:1.0')
-                header2 = paste0('browser position ',dt.bed[1,'chrom'],':',dt.bed[1,'chromStart'],'-',dt.bed[1,'chromEnd'])
-
-                system2('echo',paste0(header2,' | cat - ',file.path(control[['tempDir']],"temp.bed"),' > ',file.path(control[['tempDir']],"temp1.bed")))
-                system2('echo',paste0(header1,' | cat - ',file.path(control[['tempDir']],"temp1.bed"),' > ',as.character(filenames[mixProbSet[i]])))
-                system2('rm',paste(file.path(control[['tempDir']],"temp.bed"),file.path(control[['tempDir']],"temp1.bed")))
+                
+                writeBedGraph(dt.bed,dt.bedgraph,control,mixProbSet[i],filenames)
             }
         }
 
