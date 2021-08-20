b01eeeac17ceae0bb6e01c24e77c4521ce75963e
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-08-20 15:30:02 +1000

    Exporting segmentGenome function and fixing typo in epigraHMM.R @return field

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // aggregate
 arma::mat aggregate(arma::vec x, arma::vec f);
 RcppExport SEXP _epigraHMM_aggregate(SEXP xSEXP, SEXP fSEXP) {
