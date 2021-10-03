79702aa940c1ee1d5240baee4fbf3ea0be0b1aed
Pedro Baldoni <pedrobaldoni@gmail.com>
2021-10-03 17:39:50 +1100

    update RELEASE version number

--- src/maxStepProb.cpp
+++ src/maxStepProb.cpp
@@ -10,7 +10,29 @@ using namespace H5;
 // Enable lambda expressions.... 
 // [[Rcpp::plugins(cpp11)]]
 
-// M-step (maximization w.r.t. initial and transition probabilities)
+//' M-step (maximization w.r.t. initial and transition probabilities)
+//' @param hdf5 path to where the hdf5 is saved
+//'
+//' @examples
+//' #Creating dummy object
+//' countData <- rbind(matrix(rnbinom(1e3,mu = 2,size = 10),ncol = 1),
+//'                   matrix(rnbinom(2e3,mu = 7.5,size = 5),ncol = 1),
+//'                   matrix(rnbinom(1e3,mu = 2,size = 10),ncol = 1))
+//'
+//'
+//'
+//' colData <- data.frame(condition = 'A', replicate = 1)
+//' object <- epigraHMMDataSetFromMatrix(countData,colData)
+//'
+//' #Initializing
+//' object <- initializer(object,controlEM())
+//'
+//' #Running epigraHMM
+//' object <- epigraHMM(object,controlEM(),type = 'consensus',dist = 'nb')
+//'
+//' #Example
+//' maxStepProb(hdf5 = metadata(object)$output)
+//' @export
 //[[Rcpp::export]]
 Rcpp::List maxStepProb(Rcpp::StringVector hdf5){
     
